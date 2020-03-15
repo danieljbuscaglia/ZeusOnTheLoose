de2fba4264908ab6515307fc1bdfd09d0fefe137
Grayson Campbell <gac88@cornell.edu>
2020-03-15 17:30:01 -0400

    Fuck you dan

---
+++ card.py
@@ -0,0 +1,13 @@
+class Card():
+    def __init__(self, cardType, value):
+        self.cardType = cardType
+        self.value = value
+
+    def action(self):
+        if(self.cardType == 'god'):
+            pass
+            #TODO
+        elif(self.cardType == 'num'):
+            pass
+            #TODO
+
---
+++ hand.py
@@ -0,0 +1,9 @@
+class hand():
+    def __init__():
+        self.cards = []
+    
+    def draw():
+        newCard = #TODO
+
+        self.cards.append(newCard)
+#Some stupid shit
